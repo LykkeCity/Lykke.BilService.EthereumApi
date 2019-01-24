5dfa7ffe35a8d1b90951e91d0d25110c052338a9
Yury Glushkov <mail@glushkov.us>
2019-01-24 08:08:38 +0300

    NuGet version updated

--- src/Lykke.BilService.EthereumApi.DomainServices/EthereumAssetService.cs
+++ src/Lykke.BilService.EthereumApi.DomainServices/EthereumAssetService.cs
@@ -1,7 +1,9 @@
+using JetBrains.Annotations;
 using Lykke.Quintessence.Domain.Services;
 
 namespace Lykke.BilService.EthereumApi.Domain.Services
 {
+    [UsedImplicitly]
     public class EthereumAssetService : DefaultAssetServiceBase
     {
         public EthereumAssetService() : base(18, "", "ETH", "ETH")
